5c492ef251e23e62f2acd55341018400bb670134
Mykola Kalchuk <ul0250896@edu.uni.lodz.pl>
2021-09-20 22:31:39 +0300

    - Added new line of code in IdleManager;

--- Assets/Scripts/Managers Script/IdleManager.cs
+++ Assets/Scripts/Managers Script/IdleManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class IdleManager : MonoBehaviour
 {
@@ -21,15 +22,87 @@ public class IdleManager : MonoBehaviour
     void Awake()
     {
         if (IdleManager.instance)
-            object.Destroy(gameObject);
+            UnityEngine.Object.Destroy(gameObject);
         else
             IdleManager.instance = this;
 
-        length = PlayerPrefs.GetInt("Length", 30);
+        length = -PlayerPrefs.GetInt("Length", 30);
+        strength = PlayerPrefs.GetInt("Strength", 3);
+        offlineEarnings = PlayerPrefs.GetInt("Offline", 3);
+        lengthCost = costs[-length / 10 - 3];
+        strengthCost = costs[strength - 3];
+        offlineEarningsCost = costs[offlineEarnings - 3];
+        wallet = PlayerPrefs.GetInt("Wallet", 0);
     }
 
-    void Update()
+    private void OnApplicationPause(bool paused)
     {
-        
+        if(paused)
+        {
+            DateTime now = DataTime.Now;
+            PlayerPrefs.SetString("Date", string.Empty);
+            MonoBehaviour.print(now.ToString());
+        }
+        else
+        {
+            string @string = PlayerPrefs.GetString("Date", string.Empty);
+            if(@string != string.Empty)
+            {
+                DateTime d = DateTime.Parse(@string);
+                totalGain = (int)((DateTime.Now - d).TotalMinutes * offlineEarnings + 1.0);
+                // ScreenManager Return
+                print(totalGain);
+            }
+        }
     }
+
+    private void OnApplicationQuit()
+    {
+        OnApplicationPause(true);
+    }
+
+    public void BuyLength()
+    {
+        length -= 10;
+        wallet -= lengthCost;
+        lengthCost = costs[-length / 10 - 3];
+        PlayerPrefs.SetInt("Length", -length);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //ScreenManager MAIN
+    }
+
+    public void BuyStrength()
+    {
+        strength++;
+        wallet -= strengthCost;
+        strengthCost = costs[strength - 3];
+        PlayerPrefs.SetInt("Strength", strength);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //ScreenManager MAIN
+    }
+
+    public void BuyOfflineEarnings()
+    {
+        offlineEarnings++;
+        wallet -= offlineEarningsCost;
+        strengthCost = costs[offlineEarnings - 3];
+        PlayerPrefs.SetInt("Offline", offlineEarnings);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //ScreenManager MAIN
+    }
+
+    public void CollectMoney()
+    {
+        wallet += totalGain;
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //BackTOMAIN
+    }
+
+    public void CollectDoubleMoney()
+    {
+        wallet += totalGain * 2;
+        PlayerPrefs.SetInt("Wallet", wallet);
+        //BackTOMAIN
+    }
+
 }
